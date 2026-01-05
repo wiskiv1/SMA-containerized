bebe9353bf757e7614fc851a543c1c7d4ac0810f
wiski <32271164+wiskiv1@users.noreply.github.com>
2026-01-05 21:34:25 +0100

    sale screen logic

---
+++ public/js/adminSale.js
@@ -0,0 +1,159 @@
+/**
+ * @author Marc Bresson & Witse Panneels
+ */
+/* --- GLOBAL VARIABLES --- */
+let products;
+let interval;
+let nextInterval;
+var sale_buttons = {};
+
+/**
+ * initial setup of the page
+ */
+async function setup() {
+  let postRequest = await fetch("/api/getProducts");
+  let postResponse = await postRequest.json();
+  products = postResponse.products;
+
+  buildSaleInterface();
+
+  // handles krach
+  html_el = document.getElementsByTagName("html")[0];
+  krach_button = document.getElementById("krach");
+  krach_button.addEventListener("click", async () => {
+    fetch("/api/admin/toggleCrash");
+  });
+
+  // get interval information
+  let getRequest = await fetch("/api/getInterval");
+  let getResponse = await getRequest.json();
+  interval = getResponse.interval;
+  nextInterval = getResponse.time;
+
+  //start loop every second
+  setInterval(() => {
+    loop();
+  }, 1000);
+}
+
+/**
+ * function that is called every x seconds to update prices and other information
+ */
+function loop() {
+  const timeLeft = nextInterval - Date.now(); // time (ms) left till next interval
+
+  update_countdown_new_price(timeLeft);
+
+  if (timeLeft < 0) {
+    update_prices();
+
+    // get new interval info
+    fetch("/api/getInterval").then(async (resp) => {
+      let data = await resp.json();
+      interval = data.interval;
+      nextInterval = data.time;
+    });
+  }
+}
+
+function buildSaleInterface() {
+  // build up the admin interface
+  let el_drinks = document.getElementById("drinks");
+
+  let compteur = 0;
+  for (let product of products) {
+    let trigram = product.tri;
+    let fullname = product.name;
+    let initial_price = product.defaultPrice;
+    let colour = "hsl(" + Math.ceil((compteur * 360) / (products.length + 1)) + ", 90%, 60%)";
+    product.color = colour;
+    compteur += 1;
+
+    let bouton = new SaleButton(trigram, fullname, initial_price, colour);
+    sale_buttons[trigram] = bouton;
+
+    el_drinks.appendChild(bouton.html());
+  }
+
+  for (let trigram in sale_buttons) {
+    sale_buttons[trigram].dom.addEventListener("click", function () {
+      let actual_price = sale_buttons[trigram].actual_price;
+      newSale(trigram);
+
+      new_sale_animation(getProduct(trigram).color, actual_price);
+      sale_buttons[trigram].add_counter(trigram);
+    });
+  }
+
+  // TODO move this to somewhere more logical
+  for (let trigram in sale_buttons) {
+    sale_buttons[trigram].dom.removeAttribute("disabled");
+  }
+}
+
+async function update_prices() {
+  console.log("updateing prices");
+  let response = await fetch("/api/getCurrentPrices");
+  let prices = (await response.json()).prices;
+  console.log(prices);
+  let new_price = {};
+  for (let p of prices) {
+    new_price[p.tri] = p.price;
+  }
+
+  update_sales(new_price);
+}
+
+function update_sales(new_price) {
+  for (let drink in new_price) {
+    sale_buttons[drink].update_dom(new_price[drink]);
+  }
+}
+
+// update countdown to new price
+countdown_new_price_el = document.getElementById("remaining_time_til_new_prices");
+function update_countdown_new_price(i) {
+  const time = Math.ceil(i / 1000);
+  if (time < 0) {
+    countdown_new_price_el.innerText = "0";
+  } else {
+    countdown_new_price_el.innerText = time;
+  }
+}
+
+/**
+ * Sell a drink
+ * @param {string} tri
+ */
+function newSale(tri) {
+  fetch("/api/admin/sale", {
+    method: "POST",
+    body: JSON.stringify({
+      tri: tri,
+    }),
+  })
+    .catch((e) => {
+      console.log("Failed to sell " + tri);
+      console.log(e);
+    })
+    .then(async (response) => {
+      let json = await response.json();
+      if (!json.succes) {
+        console.log("Failed to sell " + tri);
+      }
+    });
+}
+
+/**
+ * get a drink by its trigram
+ * @param {string} tri
+ */
+function getProduct(tri) {
+  for (let drink of products) {
+    if (drink.tri == tri) {
+      return drink;
+    }
+  }
+}
+
+setup();
--- src/app/admin/sale/page.tsx
+++ src/app/admin/sale/page.tsx
@@ -3,6 +3,7 @@
  * @date 2026-01-02
  */
 import "./page.css";
+import Script from "next/script";
 
 export default function Sale() {
   return (
@@ -18,25 +19,10 @@ export default function Sale() {
         </div>
         <div id="krach">Krach</div>
       </div>
-      <div id="drinks">
-        <div className="drink" id="jup">
-          <div className="name">
-            <span className="trigram">JUP</span> - <span>Jupiler</span>
-          </div>
-          <div className="infos">
-            <div className="prices">
-              <span className="actual_price">€2.00</span>
-              <div>
-                <div className="initial_price">€2.50</div>
-                <div className="variation">-20%</div>
-              </div>
-            </div>
-            <div className="add_sale" style={{ backgroundColor: "red" }}>
-              +1
-            </div>
-          </div>
-        </div>
-      </div>
+      <div id="drinks"></div>
+      <Script src="/js/sale_animation.js" />
+      <Script src="/js/sale_button.js" />
+      <Script src="/js/adminSale.js" />
     </div>
   );
 }
