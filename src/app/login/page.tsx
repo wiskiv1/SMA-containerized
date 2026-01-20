b085108228c94f705d75fee3de75a089dd30398d
wiski <32271164+wiskiv1@users.noreply.github.com>
2026-01-20 14:29:24 +0100

    fix callback search param

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,25 +1,32 @@
 "use client";
 
 import { signIn } from "next-auth/react";
+import { NextRequest } from "next/server";
 import { useState } from "react";
+import { useSearchParams } from "next/navigation";
+import "./page.css";
 
 export default function LoginPage() {
-  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+
+  const searchParams = useSearchParams();
+  const callback = searchParams.get("callbackUrl");
+  console.log(callback);
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
 
     await signIn("credentials", {
-      email,
+      username,
       password,
-      callbackUrl: "/admin",
+      callbackUrl: callback ? callback : "/admin",
     });
   };
 
   return (
     <form onSubmit={handleSubmit}>
-      <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+      <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} required />
       <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
       <button type="submit">Sign in</button>
     </form>
