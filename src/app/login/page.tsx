12be493b2ab11c1095244dc4ac04792a92a13e6c
wiski <32271164+wiskiv1@users.noreply.github.com>
2026-01-20 15:16:03 +0100

    pretty login/logout page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,12 +1,15 @@
 "use client";
-
-import { signIn } from "next-auth/react";
-import { NextRequest } from "next/server";
+/**
+ * @author Witse Panneels
+ */
+import { signIn, signOut, useSession } from "next-auth/react";
 import { useState } from "react";
 import { useSearchParams } from "next/navigation";
 import "./page.css";
 
 export default function LoginPage() {
+  const { data: session, status } = useSession();
+
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
@@ -14,7 +17,7 @@ export default function LoginPage() {
   const callback = searchParams.get("callbackUrl");
   console.log(callback);
 
-  const handleSubmit = async (e: React.FormEvent) => {
+  const handleLogin = async (e: React.FormEvent) => {
     e.preventDefault();
 
     await signIn("credentials", {
@@ -24,11 +27,37 @@ export default function LoginPage() {
     });
   };
 
+  const handleLogout = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    await signOut();
+  };
+
   return (
-    <form onSubmit={handleSubmit}>
-      <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} required />
-      <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
-      <button type="submit">Sign in</button>
-    </form>
+    <div id="content">
+      <form id="form_container" onSubmit={session ? handleLogout : handleLogin}>
+        <h1>Login</h1>
+        <div id="input_container">
+          {status === "loading" ? (
+            <h2>Loading</h2>
+          ) : session ? (
+            <>
+              <h3>logged in as:</h3>
+              <h2>{session.user.name}</h2>
+            </>
+          ) : (
+            <>
+              <h2>Username</h2>
+              <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} required />
+              <h2>Password</h2>
+              <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+            </>
+          )}
+        </div>
+        <button id="submitButton" type="submit">
+          {session ? "Sign Out" : "Sign in"}
+        </button>
+      </form>
+    </div>
   );
 }
