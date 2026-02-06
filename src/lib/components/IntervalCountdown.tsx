9c40a63241b0253ffc297bc21b650f2caae86d4f
wiski <32271164+wiskiv1@users.noreply.github.com>
2026-02-06 22:24:07 +0100

    lolz

--- src/lib/components/IntervalCountdown.tsx
+++ src/lib/components/IntervalCountdown.tsx
@@ -3,22 +3,18 @@
  * @author Witse Panneels
  */
 import { useEffect, useContext, useRef, useState } from "react";
-import { CrashContext, NextIntervalContext } from "./sale/InervalContext";
+import { NextIntervalContext } from "./sale/InervalContext";
 
 export default function IntervalCountdown() {
   const nextInterval = useContext(NextIntervalContext);
   const nextIntervalRef = useRef(nextInterval);
-  const crash = useContext(CrashContext);
-  const crashRef = useRef(crash); // always in sync with context
-  const is_krach = useRef(crash); // manually update
 
   const [countdown, updateCountdown] = useState(10);
 
   // protection against stale variables
   useEffect(() => {
     nextIntervalRef.current = nextInterval;
-    crashRef.current = crash;
-  }, [nextInterval, crash]);
+  }, [nextInterval]);
 
   useEffect(() => {
     const intervalId = setInterval(async () => {
